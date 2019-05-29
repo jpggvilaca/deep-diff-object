ac460bda788eea1c863078120145adf47f05f129
jpggvilaca <jpggvilaca@gmail.com>
2019-05-29 20:54:23 +0200

    Add the rest of comments/documentation; Add TODO

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
-/** 
+/**
  * @description Check value's data type (object, string, number, function)
  * @param {any} data Value with any type
- * 
+ *
  * @return {string} The type of the element
  */
 const checkType = function(data) {
@@ -9,18 +9,30 @@ const checkType = function(data) {
   const type = typeString[1].slice(0, -1);
 
   return type;
-}
+};
 
-/** 
+/**
  * @description Check if the object has a certain key
  * @param {object} obj A javascript object
  * @param {object} obj A javascript object
- * 
+ *
  * @return {boolean} If the object has or not a certain key
  */
 const hasOwn = function(obj, key) {
+  if (!obj || !key) {
+    throw new Error('Insufficient arguments passed to "hasOwn" func');
+  }
+
+  if (typeof obj !== 'object' || typeof key !== 'string') {
+    throw new TypeError(
+      `${obj} should be of type 'object' and ${key} should be of type 'string'`,
+      'helpers.js',
+      23
+    );
+  }
+
   return obj.hasOwnProperty(key);
-}
+};
 
 /** 
  * @description Check if two values are deepEqual regardless of their types. 'key' here is an
@@ -28,7 +40,7 @@ const hasOwn = function(obj, key) {
   differences from them but we also need to know the name of the attribute itself.
  * @param {any} element1 Value with any type
  * @param {any} element2 Value with any type
- * @param {function} callback TBD
+ * @param {function} callback For recursive calls
  * @param {string} key Value with any type
  * 
  * @return {boolean} If the object has or not a certain key
@@ -38,30 +50,34 @@ const isEqual = function(value1, value2, callback, key) {
   const type = checkType(value1);
 
   // If the element is an array
-  if(type === 'Array') {
+  if (type === 'Array') {
     // If the arrays differ in size, it's considered different
-    if (value1.length !== value2.length) { return false; }
+    if (value1.length !== value2.length) {
+      return false;
+    }
 
-    
     for (let x = 0; x < value1.length; x++) {
       // If the element is an object, we need to recursively check that object
       if (checkType(value1[x]) === 'Object' && callback) {
         const result = callback(value1[x], value2[x], key);
 
-        if(!result.one.length) { return true; }
-      }
-
-      else {
+        if (!result.one.length) {
+          return true;
+        }
+      } else {
         for (let x = 0; x < value1.length; x++) {
           // If the element is a function, we just check the length of that function
           // when converted to a string
-          if (checkType(value1[x] === 'Function') && value1.toString() !== value2.toString()) {
+          if (
+            checkType(value1[x] === 'Function') &&
+            value1.toString() !== value2.toString()
+          ) {
             return false;
           }
 
           if (value1[x].toString() !== value2[x].toString()) {
             return false;
-          };
+          }
         }
       }
     }
@@ -71,16 +87,16 @@ const isEqual = function(value1, value2, callback, key) {
   else if (type === 'Object' && callback) {
     const result = callback(value1, value2, key);
 
-    if(!result.one.length) { return true; }
-  }
-
-  else {
-    if(value1.toString() !== value2.toString()) {
+    if (!result.one.length) {
+      return true;
+    }
+  } else {
+    if (value1.toString() !== value2.toString()) {
       return false;
     }
   }
 
   return true;
-}
+};
 
 module.exports = { hasOwn, isEqual };
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/** 
+ * TODO
+ * - Abstract each type comparison
+ * - Add tests
+*/
 const h = require('./helpers');
 
 const result = {
@@ -18,7 +23,8 @@ const deepDiff = function(objA, objB, key) {
       h.hasOwn(objB, keysA[i]) &&
       !h.isEqual(objA[keysA[i]], objB[keysA[i]], deepDiff, keysA[i])
     ) {
-      // If ... (TBD)
+      // If `key` is set it means it's a recursive call so we append the result
+      // of the comparison to that `key`
       if (key) {
         result.one.push({ [key]: { [keysA[i]]: objA[keysA[i]] }});
         result.two.push({ [key]: { [keysA[i]]: objB[keysA[i]] }});
